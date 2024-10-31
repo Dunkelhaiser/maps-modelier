8d40808aa944d0f43d23ef6585337920b2a2a2a1
Dunkelhaiser <103267805+Dunkelhaiser@users.noreply.github.com>
2024-10-31 16:46:00 +0200

    feat: add maps table

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,4 +1,14 @@
-import { pgTable, serial, text } from "drizzle-orm/pg-core";
+import { pgTable, serial, text, timestamp, uuid } from "drizzle-orm/pg-core";
+
+export const maps = pgTable("maps", {
+    id: uuid("id").primaryKey().defaultRandom(),
+    name: text("name").notNull(),
+    createdAt: timestamp("createdAt").notNull().defaultNow(),
+    updatedAt: timestamp("updatedAt")
+        .notNull()
+        .defaultNow()
+        .$onUpdate(() => new Date()),
+});
 
 export const provinces = pgTable("provinces", {
     id: serial("id").primaryKey(),
