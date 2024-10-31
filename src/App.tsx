a9fbea980ecb870dc1c9ff7657a5bab19bc12583
Dunkelhaiser <103267805+Dunkelhaiser@users.noreply.github.com>
2024-10-31 16:49:43 +0200

    feat: add tables initialization

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,14 @@ import { MapCanvas } from "@components/Map";
 import { MapUpload } from "@components/MapUpload";
 import { useEffect, useState } from "react";
 import { sql } from "./db/db";
+import { sqlSchema } from "./db/schema.sql";
 
 const App = () => {
     const [imageUrl, setImageUrl] = useState<string | null>(null);
 
     useEffect(() => {
         const query = async () => {
-            await sql.exec(
-                `CREATE TABLE IF NOT EXISTS "provinces" ("id" serial PRIMARY KEY NOT NULL, "color" text NOT NULL, "type" text NOT NULL, CONSTRAINT "provinces_color_unique" UNIQUE("color"));`
-            );
+            await sql.exec(sqlSchema);
         };
 
         query();
---
+++ src/db/schema.sql.ts
@@ -0,0 +1,16 @@
+export const sqlSchema = `
+    CREATE TABLE IF NOT EXISTS "provinces" (
+        "id" serial PRIMARY KEY NOT NULL,
+        "color" text NOT NULL,
+        "type" text NOT NULL,
+        CONSTRAINT "provinces_color_unique" UNIQUE("color")
+    );
+    
+    CREATE TABLE IF NOT EXISTS "maps" (
+        "id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+        "name" text NOT NULL,
+        "createdAt" timestamp DEFAULT now() NOT NULL,
+        "updatedAt" timestamp DEFAULT now() NOT NULL
+    );
+
+`;
