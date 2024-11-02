40adbd8e013ac6ecf4a86641948379466f78d8c4
Dunkelhaiser <103267805+Dunkelhaiser@users.noreply.github.com>
2024-11-02 20:41:44 +0200

    deps: remove pglite

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,6 @@ import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
     plugins: [react(), tsconfigPaths()],
-    assetsInclude: ["**/*.sql"],
-    optimizeDeps: {
-        exclude: ["@electric-sql/pglite"],
-    },
     server: {
         port: 5173,
         strictPort: true,
