4675e3e556d1c8a3e6e1ccfb884de023dd5bcd60
Dunkelhaiser <103267805+Dunkelhaiser@users.noreply.github.com>
2024-11-12 21:34:24 +0200

    perf: increase unique color selection with bitwise operators

--- src/electron/lib/provinces/processProvinces.ts
+++ src/electron/lib/provinces/processProvinces.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-bitwise */
 import { ImageData, createCanvas, loadImage } from "@napi-rs/canvas";
 import { eq } from "drizzle-orm";
 import { db } from "../../db/db.js";
@@ -13,15 +14,33 @@ export const processProvinces = async (_: Electron.IpcMainInvokeEvent, imageData
     const ctx = canvas.getContext("2d");
 
     ctx.drawImage(image, 0, 0);
+    ctx.imageSmoothingEnabled = false;
+    ctx.willReadFrequently = true;
 
     // eslint-disable-next-line @typescript-eslint/no-unsafe-call
     const imageDataObj = ctx.getImageData(0, 0, canvas.width, canvas.height) as ImageData;
-    const { data } = imageDataObj;
+    const { data, width } = imageDataObj;
 
-    const uniqueColors = new Set<string>();
-    for (let i = 0; i < data.length; i += 4) {
-        uniqueColors.add(rgbToHex(data[i], data[i + 1], data[i + 2]));
+    const colorSet = new Set<number>();
+    const rowWidth4 = width * 4;
+
+    for (let i = 0, rowOffset = 0; i < canvas.height; i++, rowOffset += rowWidth4) {
+        for (let x = 0, pixelOffset = rowOffset; x < width; x++, pixelOffset += 4) {
+            const r = data[pixelOffset];
+            const g = data[pixelOffset + 1];
+            const b = data[pixelOffset + 2];
+            const colorKey = (r << 16) | (g << 8) | b;
+            colorSet.add(colorKey);
+        }
     }
+
+    const uniqueColors = Array.from(colorSet).map((color) => {
+        const r = (color >> 16) & 255;
+        const g = (color >> 8) & 255;
+        const b = color & 255;
+
+        return rgbToHex(r, g, b);
+    });
 
     await db.transaction(async (tx) => {
         const provinceValues = Array.from(uniqueColors).map((color, index) => ({
@@ -30,7 +49,9 @@ export const processProvinces = async (_: Electron.IpcMainInvokeEvent, imageData
             color,
         }));
 
-        await tx.insert(provinces).values(provinceValues);
+        for (let i = 0; i < provinceValues.length; i += 1000) {
+            await tx.insert(provinces).values(provinceValues.slice(i, i + 1000));
+        }
     });
 
     const mapProvinces = await db
