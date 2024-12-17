433b4e2bbae2f139acb1c6d18ffefd66fcb0f05a
Dunkelhaiser <103267805+Dunkelhaiser@users.noreply.github.com>
2024-12-17 20:06:55 +0200

    feat: add maps control buttons

--- src/client/components/MapButton.tsx
+++ src/client/components/MapButton.tsx
@@ -1,6 +1,6 @@
 import { Button } from "@ui/Button";
 import { Map } from "@utils/types";
-import { MapIcon } from "lucide-react";
+import { Edit, MapIcon, Trash } from "lucide-react";
 import { useState } from "react";
 import { toast } from "sonner";
 import UploadMapImageDialog from "./Dialogs/UploadMapImageDialog";
@@ -30,20 +30,33 @@ const MapButton = ({ map }: Props) => {
 
     return (
         <>
-            <Button
-                key={map.id}
-                variant="outline"
-                className="h-auto w-full justify-start"
-                onClick={handleExistingMapSelect}
-            >
+            <div className="relative flex h-auto w-full cursor-pointer items-center justify-center gap-2 rounded-md border border-input bg-background px-4 py-2 shadow-sm transition focus-within:translate-y-[2px] focus-within:ring-2 focus-within:ring-ring hover:translate-y-[2px] hover:bg-accent hover:text-accent-foreground">
                 <MapIcon className="mr-2 size-5" />
                 <div className="flex flex-col items-start">
-                    <span>{map.name}</span>
+                    <button
+                        type="button"
+                        onClick={handleExistingMapSelect}
+                        className="text-sm font-medium outline-none before:absolute before:inset-0 before:content-['']"
+                    >
+                        {map.name}
+                    </button>
                     <span className="text-xs text-muted-foreground">
                         Created: {new Date(map.createdAt).toLocaleDateString()}
                     </span>
                 </div>
-            </Button>
+                <div className="z-10 ml-auto space-x-0.5">
+                    <Button variant="ghost" size="icon" className="hover:bg-white/75">
+                        <Edit />
+                    </Button>
+                    <Button
+                        variant="ghost"
+                        size="icon"
+                        className="text-destructive hover:bg-red-100/45 hover:text-destructive"
+                    >
+                        <Trash />
+                    </Button>
+                </div>
+            </div>
             <UploadMapImageDialog
                 selectedMapForUpload={selectedMapForUpload}
                 setSelectedMapForUpload={setSelectedMapForUpload}
