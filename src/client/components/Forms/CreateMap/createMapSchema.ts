058418b29f949d97257ec947365d7865aabca719
Dunkelhaiser <103267805+Dunkelhaiser@users.noreply.github.com>
2024-12-17 22:26:49 +0200

    chore: make validation errors more descriptive

--- src/client/components/Forms/CreateMap/createMapSchema.ts
+++ src/client/components/Forms/CreateMap/createMapSchema.ts
@@ -2,7 +2,11 @@ import { z as zod } from "zod";
 import { imageSchema } from "../sharedSchemas";
 
 export const createMapSchema = zod.object({
-    name: zod.string().trim().min(1).max(50),
+    name: zod
+        .string()
+        .trim()
+        .min(1, { message: "Provide map name" })
+        .max(50, { message: "Map name should be less than 50 characters" }),
     provinces: imageSchema,
 });
 
--- src/client/components/Forms/RenameMap/renameMapSchema.ts
+++ src/client/components/Forms/RenameMap/renameMapSchema.ts
@@ -1,7 +1,11 @@
 import { z as zod } from "zod";
 
 export const renameMapSchema = zod.object({
-    name: zod.string().trim().min(1).max(50),
+    name: zod
+        .string()
+        .trim()
+        .min(1, { message: "Provide map name" })
+        .max(50, { message: "Map name should be less than 50 characters" }),
 });
 
 export type RenameMapInput = zod.infer<typeof renameMapSchema>;
