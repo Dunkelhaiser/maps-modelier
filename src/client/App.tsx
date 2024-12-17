efd336f96f20029d669f136dc52ba404a0c60001
Dunkelhaiser <103267805+Dunkelhaiser@users.noreply.github.com>
2024-12-17 23:50:25 +0200

    fix: move toaster out of main grid

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -7,10 +7,12 @@ const App = () => {
     const activeMap = useMapStore((state) => state.activeMap);
 
     return (
-        <main className="grid h-screen w-screen place-items-center">
+        <>
+            <main className="grid h-screen w-screen place-items-center">
+                {!activeMap ? <MapSelection /> : <MapView activeMap={activeMap} />}
+            </main>
             <Toaster />
-            {!activeMap ? <MapSelection /> : <MapView activeMap={activeMap} />}
-        </main>
+        </>
     );
 };
 
