db6e3004ecf6aac1bef39d82eafa4214a9a6b781
Dunkelhaiser <103267805+Dunkelhaiser@users.noreply.github.com>
2024-12-28 12:28:04 +0200

    feat: add query for provinces retrieval

--- src/client/ipc/province.ts
+++ src/client/ipc/province.ts
@@ -1,13 +1,24 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { EthnicityPopulation } from "@utils/types";
 import { toast } from "sonner";
 
 export const useAddPopulation = (mapId: string, provinceId: number) => {
+    const queryClient = useQueryClient();
+
     return useMutation({
-        mutationFn: async (ethnicitiesPopulation: EthnicityPopulation[]) =>
-            await window.electronAPI.addPopulation(mapId, provinceId, ethnicitiesPopulation),
+        mutationFn: async (ethnicitiesPopulation: EthnicityPopulation[]) => {
+            await window.electronAPI.addPopulation(mapId, provinceId, ethnicitiesPopulation);
+            queryClient.invalidateQueries({ queryKey: [mapId, "provinces", "land"] });
+        },
         onSuccess: () => {
             toast.success("Population added successfully");
         },
+    });
+};
+
+export const useGetProvinces = (mapId: string, type: "land" | "water") => {
+    return useQuery({
+        queryKey: [mapId, "provinces", type],
+        queryFn: async () => await window.electronAPI.getAllProvinces(mapId, type),
     });
 };
