5c8c8fbf32a3c7d85e8b4acd107ccc8fc2b9bbdb
Dunkelhaiser <103267805+Dunkelhaiser@users.noreply.github.com>
2024-12-30 14:59:51 +0200

    refactor: remove unnecessary global states

--- src/client/store/provinces.ts
+++ src/client/store/provinces.ts
@@ -4,7 +4,6 @@ import { AppStore } from "./store";
 
 export interface ProvincesSlice {
     selectedProvinces: Province[];
-    setSelectedProvinces: (province: Province, isShiftKey: boolean) => void;
     deselectProvinces: () => void;
 }
 
@@ -12,88 +11,7 @@ export const initialProvincesSlice = {
     selectedProvinces: [],
 };
 
-export const createProvincesSlice: StateCreator<AppStore, [], [], ProvincesSlice> = (set, get) => ({
+export const createProvincesSlice: StateCreator<AppStore, [], [], ProvincesSlice> = (set) => ({
     ...initialProvincesSlice,
-    setSelectedProvinces: (province: Province, isShiftKey: boolean) =>
-        set((state) => {
-            const { mode } = state;
-            let { selectedState, selectedCountry } = state;
-
-            if (selectedCountry && isShiftKey && mode === "countries_editing") {
-                const stateToRemove = state.states.find((s) => s.provinces.includes(province.id));
-
-                if (stateToRemove && selectedCountry.states.includes(stateToRemove.id)) {
-                    get().removeStatesFromCountry(selectedCountry.tag, [stateToRemove.id]);
-                    return {};
-                }
-
-                const stateToAdd = state.states.find((s) => s.provinces.includes(province.id));
-
-                if (stateToAdd) {
-                    get().addStatesToCountry(selectedCountry.tag, [stateToAdd.id]);
-                }
-                return {};
-            }
-
-            if (selectedState && isShiftKey && mode === "states_editing") {
-                const isProvinceInState = selectedState.provinces.includes(province.id);
-
-                if (isProvinceInState) {
-                    get().removeProvincesFromState(selectedState.id, [province.id]);
-                } else {
-                    get().addProvincesToState([province.id]);
-                }
-                return {};
-            }
-
-            let selectedProvinces: Province[];
-
-            if (!isShiftKey) {
-                selectedProvinces = [province];
-                selectedState = state.states.find((s) => s.provinces.includes(province.id)) ?? null;
-
-                if (selectedState) {
-                    selectedCountry =
-                        state.countries.find((country) => country.states.includes(selectedState!.id)) ?? null;
-                } else {
-                    selectedCountry = null;
-                }
-
-                return {
-                    selectedProvinces,
-                    selectedState,
-                    selectedCountry,
-                };
-            }
-
-            const isSelected = state.selectedProvinces.some((p) => p.id === province.id);
-            if (isSelected) {
-                selectedProvinces = state.selectedProvinces.filter((p) => p.id !== province.id);
-            } else {
-                selectedProvinces = [...state.selectedProvinces, province];
-            }
-
-            if (selectedProvinces.length === 0) {
-                selectedState = null;
-                selectedCountry = null;
-            } else if (!selectedState && selectedProvinces.length > 0) {
-                selectedState =
-                    state.states.find((s) => selectedProvinces.every((p) => s.provinces.includes(p.id))) ?? null;
-
-                if (selectedState) {
-                    selectedCountry =
-                        state.countries.find((country) => country.states.includes(selectedState!.id)) ?? null;
-                } else {
-                    selectedCountry = null;
-                }
-            }
-
-            return {
-                selectedProvinces,
-                selectedState,
-                selectedCountry,
-            };
-        }),
-
     deselectProvinces: () => set({ selectedProvinces: [], selectedState: null }),
 });
--- src/client/store/states.ts
+++ src/client/store/states.ts
@@ -3,71 +3,13 @@ import { StateCreator } from "zustand";
 import { AppStore } from "./store";
 
 export interface StatesSlice {
-    states: State[];
-    setStates: (states: State[]) => void;
     selectedState: State | null;
-    addProvincesToState: (provinceIds: number[]) => void;
-    removeProvincesFromState: (stateId: number, provinceIds: number[]) => void;
 }
 
 export const initialStatesSlice = {
-    states: [],
     selectedState: null,
 };
 
-export const createStatesSlice: StateCreator<AppStore, [], [], StatesSlice> = (set, get) => ({
+export const createStatesSlice: StateCreator<AppStore, [], [], StatesSlice> = () => ({
     ...initialStatesSlice,
-    setStates: (states: State[]) => set({ states }),
-    addProvincesToState: async (provinceIds: number[]) => {
-        const { activeMap, selectedState } = get();
-
-        if (!activeMap || !selectedState) return;
-
-        await window.electronAPI.addProvinces(activeMap.id, selectedState.id, provinceIds);
-
-        const updatedState = {
-            ...selectedState,
-            provinces: [...new Set([...selectedState.provinces, ...provinceIds])],
-        };
-
-        set((state) => ({
-            states: state.states.map((s) => {
-                if (s.id === selectedState.id) {
-                    return updatedState;
-                }
-                if (s.provinces.some((p) => provinceIds.includes(p))) {
-                    return {
-                        ...s,
-                        provinces: s.provinces.filter((p) => !provinceIds.includes(p)),
-                    };
-                }
-                return s;
-            }),
-            selectedState: updatedState,
-        }));
-    },
-    removeProvincesFromState: async (stateId, provinceIds) => {
-        const { activeMap, selectedState } = get();
-
-        if (!activeMap || !selectedState) return;
-
-        await window.electronAPI.removeProvinces(activeMap.id, stateId, provinceIds);
-
-        const updatedState = {
-            ...selectedState,
-            provinces: selectedState.provinces.filter((id) => !provinceIds.includes(id)),
-        };
-
-        set((state) => {
-            const remainingSelectedProvinces = state.selectedProvinces.filter(
-                (province) => !provinceIds.includes(province.id)
-            );
-
-            return {
-                states: state.states.map((s) => (s.id === selectedState.id ? updatedState : s)),
-                selectedState: remainingSelectedProvinces.length > 0 ? updatedState : null,
-                selectedProvinces: remainingSelectedProvinces,
-            };
-        });
-    },
 });
