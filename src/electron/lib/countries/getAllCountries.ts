3a628dffa6e67d03283892457e50cf35a85be05d
Dunkelhaiser <103267805+Dunkelhaiser@users.noreply.github.com>
2025-01-03 13:10:48 +0200

    feat: add total population and ethnic composition of countries

--- src/electron/lib/countries/getAllCountries.ts
+++ src/electron/lib/countries/getAllCountries.ts
@@ -1,17 +1,87 @@
-import { eq, sql } from "drizzle-orm";
+import { and, desc, eq, sql, sum } from "drizzle-orm";
 import { db } from "../../db/db.js";
-import { countries, countryStates } from "../../db/schema.js";
+import { countries, countryStates, states, stateProvinces, provincePopulations, ethnicities } from "../../db/schema.js";
+
+interface Ethnicity {
+    id: number;
+    name: string;
+    population: number;
+}
 
 export const getAllCountries = async (_: Electron.IpcMainInvokeEvent, mapId: string) => {
+    const ethnicityTotals = db.$with("ethnicity_totals").as(
+        db
+            .select({
+                countryTag: countries.tag,
+                ethnicityId: ethnicities.id,
+                ethnicityName: ethnicities.name,
+                totalPopulation: sum(provincePopulations.population).as("total_population"),
+            })
+            .from(countries)
+            .leftJoin(countryStates, eq(countryStates.countryTag, countries.tag))
+            .leftJoin(states, eq(states.id, countryStates.stateId))
+            .leftJoin(stateProvinces, eq(stateProvinces.stateId, states.id))
+            .leftJoin(
+                provincePopulations,
+                and(
+                    eq(provincePopulations.provinceId, stateProvinces.provinceId),
+                    eq(provincePopulations.mapId, stateProvinces.mapId)
+                )
+            )
+            .leftJoin(
+                ethnicities,
+                and(
+                    eq(ethnicities.id, provincePopulations.ethnicityId),
+                    eq(ethnicities.mapId, provincePopulations.mapId)
+                )
+            )
+            .where(eq(countries.mapId, mapId))
+            .groupBy(countries.tag, ethnicities.id, ethnicities.name)
+            .orderBy(desc(sum(provincePopulations.population)))
+    );
+
+    const countryEthnicities = db.$with("country_ethnicities").as(
+        db
+            .select({
+                countryTag: ethnicityTotals.countryTag,
+                ethnicityData: sql<string>`
+                json_group_array(
+                    json_object(
+                        'id', ${ethnicityTotals.ethnicityId},
+                        'name', ${ethnicityTotals.ethnicityName},
+                        'population', ${ethnicityTotals.totalPopulation}
+                    )
+                ) FILTER (WHERE ${ethnicityTotals.ethnicityId} IS NOT NULL AND ${ethnicityTotals.totalPopulation} > 0)
+            `.as("ethnicity_data"),
+            })
+            .from(ethnicityTotals)
+            .groupBy(ethnicityTotals.countryTag)
+    );
+
+    // Main query
     const countriesArr = await db
+        .with(ethnicityTotals, countryEthnicities)
         .select({
             tag: countries.tag,
             name: countries.name,
             color: countries.color,
             states: sql<string>`COALESCE(GROUP_CONCAT(${countryStates.stateId}), '')`,
+            population: sql<number>`
+                COALESCE((
+                    SELECT SUM(${provincePopulations.population})
+                    FROM ${provincePopulations}
+                    JOIN ${stateProvinces} ON ${stateProvinces.provinceId} = ${provincePopulations.provinceId}
+                    JOIN ${states} ON ${states.id} = ${stateProvinces.stateId}
+                    JOIN ${countryStates} ON ${countryStates.stateId} = ${states.id}
+                    WHERE ${countryStates.countryTag} = ${countries.tag}
+                    AND ${provincePopulations.mapId} = ${countries.mapId}
+                ), 0)
+            `.mapWith(Number),
+            ethnicities: countryEthnicities.ethnicityData,
         })
         .from(countries)
         .leftJoin(countryStates, eq(countryStates.countryTag, countries.tag))
+        .leftJoin(countryEthnicities, eq(countryEthnicities.countryTag, countries.tag))
         .where(eq(countries.mapId, mapId))
         .groupBy(countries.tag)
         .orderBy(countries.tag);
@@ -19,5 +89,8 @@ export const getAllCountries = async (_: Electron.IpcMainInvokeEvent, mapId: str
     return countriesArr.map((country) => ({
         ...country,
         states: country.states ? country.states.split(",").map(Number) : [],
+        ethnicities: JSON.parse(country.ethnicities as unknown as string) as (Omit<Ethnicity, "id"> & {
+            id: number | null;
+        })[],
     }));
 };
