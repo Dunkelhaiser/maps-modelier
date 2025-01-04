371771c777b7e02662bc7957c97aa1f66e7ca5a8
Dunkelhaiser <103267805+Dunkelhaiser@users.noreply.github.com>
2025-01-04 14:50:02 +0200

    refactor: add generic file schema

--- src/client/utils/sharedSchemas.ts
+++ src/client/utils/sharedSchemas.ts
@@ -3,62 +3,70 @@ import { z as zod } from "zod";
 const MAX_FILE_SIZE = 500000000;
 const ACCEPTED_IMAGE_TYPES = ["image/jpeg", "image/jpg", "image/png", "image/webp", "image/bmp"];
 
-export const imageSchema = zod
-    .unknown()
-    .transform((value) => value as File[])
-    .superRefine((files, ctx) => {
-        if (files.length !== 1) {
-            ctx.addIssue({
-                code: zod.ZodIssueCode.custom,
-                message: "Provide an image",
-                fatal: true,
+interface FileOptions {
+    maxSize?: number;
+    acceptedTypes: string[];
+    emptyMessage?: string;
+    sizeMessage?: string;
+    typesMessage?: string;
+}
+
+const isFileTypeMatch = (fileType: string, acceptedType: string) => {
+    if (acceptedType.endsWith("/*")) {
+        const [mainType] = acceptedType.split("/");
+        return fileType.startsWith(`${mainType}/`);
+    }
+
+    return fileType === acceptedType;
+};
+
+export const fileSchema = ({
+    maxSize = MAX_FILE_SIZE,
+    acceptedTypes,
+    emptyMessage = "Provide a file",
+    sizeMessage = `Max size is ${maxSize}MB`,
+    typesMessage = "Provide a file in the accepted format",
+}: FileOptions) =>
+    zod
+        .unknown()
+        .transform((value) => value as File[])
+        .superRefine((files, ctx) => {
+            if (files.length !== 1) {
+                ctx.addIssue({
+                    code: zod.ZodIssueCode.custom,
+                    message: emptyMessage,
+                    fatal: true,
+                });
+
+                return zod.NEVER;
+            }
+            return true;
+        })
+        .refine((files) => files[0].size <= MAX_FILE_SIZE, sizeMessage)
+        .refine((files) => acceptedTypes.some((type) => isFileTypeMatch(files[0].type, type)), typesMessage)
+        .transform((files) => files[0])
+        .transform((file) => {
+            const reader = new FileReader();
+            reader.readAsDataURL(file);
+            return new Promise<string>((resolve, reject) => {
+                reader.onload = () => resolve(reader.result as string);
+                reader.onerror = reject;
             });
-
-            return zod.NEVER;
-        }
-        return true;
-    })
-    .refine((files) => files[0].size <= MAX_FILE_SIZE, "Max image size is 500MB")
-    .refine(
-        (files) => ACCEPTED_IMAGE_TYPES.includes(files[0].type),
-        "Provide an image in .jpg, .jpeg, .png or .webp format"
-    )
-    .transform((files) => files[0])
-    .transform((file) => {
-        const reader = new FileReader();
-        reader.readAsDataURL(file);
-        return new Promise<string>((resolve, reject) => {
-            reader.onload = () => resolve(reader.result as string);
-            reader.onerror = reject;
         });
-    });
 
-export const audioSchema = zod
-    .unknown()
-    .transform((value) => value as File[])
-    .superRefine((files, ctx) => {
-        if (files.length !== 1) {
-            ctx.addIssue({
-                code: zod.ZodIssueCode.custom,
-                message: "Provide an audio",
-                fatal: true,
-            });
+export const imageSchema = fileSchema({
+    acceptedTypes: ACCEPTED_IMAGE_TYPES,
+    typesMessage: "Provide an image in .jpg, .jpeg, .png, .webp or .bmp format",
+    sizeMessage: "Max image size is 500MB",
+    emptyMessage: "Provide an image",
+});
 
-            return zod.NEVER;
-        }
-        return true;
-    })
-    .refine((files) => files[0].size <= MAX_FILE_SIZE, "Max audio size is 500MB")
-    .refine((files) => files[0].type.includes("audio"), "Provide an audio file")
-    .transform((files) => files[0])
-    .transform((file) => {
-        const reader = new FileReader();
-        reader.readAsDataURL(file);
-        return new Promise<string>((resolve, reject) => {
-            reader.onload = () => resolve(reader.result as string);
-            reader.onerror = reject;
-        });
-    });
+export const audioSchema = fileSchema({
+    acceptedTypes: ["audio/*"],
+    typesMessage: "Provide an audio file",
+    sizeMessage: "Max audio size is 500MB",
+    emptyMessage: "Provide an audio",
+});
 
 export const nameSchema = zod.object({
     name: zod
