2bae6be235286f85de6370429443cce7d5f56795
Dunkelhaiser <103267805+Dunkelhaiser@users.noreply.github.com>
2025-01-15 17:34:10 +0200

    feat: add shortcut for mode change

--- src/client/screens/MapView/MapView.tsx
+++ src/client/screens/MapView/MapView.tsx
@@ -1,12 +1,19 @@
 import { MapCanvas } from "@screens/MapView/components/Map/Map";
 import UserInterface from "@screens/MapView/components/UserInterface/UserInterface";
+import { useMapStore } from "@store/store";
 import { ActiveMap } from "@utils/types";
+import { useHotkeys } from "react-hotkeys-hook";
 
 interface Props {
     activeMap: ActiveMap;
 }
 
 const MapView = ({ activeMap }: Props) => {
+    const mode = useMapStore((state) => state.mode);
+    const setMode = useMapStore((state) => state.setMode);
+
+    useHotkeys("ctrl+e", () => (mode === "editing" ? setMode("viewing") : setMode("editing")));
+
     return (
         <>
             <UserInterface />
