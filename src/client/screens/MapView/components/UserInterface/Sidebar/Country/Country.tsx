3296a3a6da0686e01929a5c2f4ae1f74ddf2dd96
Dunkelhaiser <103267805+Dunkelhaiser@users.noreply.github.com>
2025-03-05 12:15:23 +0200

    feat: add country sidebar

---
+++ src/client/screens/MapView/components/UserInterface/Sidebar/Country/Country.tsx
@@ -0,0 +1,19 @@
+import CardHeaderWithClose from "@components/CardHeaderWithClose";
+import { useSidebarStore } from "@store/sidebar";
+import { useMapStore } from "@store/store";
+import { CardContent, CardTitle } from "@ui/Card";
+
+const Country = () => {
+    const selectedCountry = useMapStore((state) => state.selectedCountry);
+    const closeSidebar = useSidebarStore((state) => state.closeSidebar);
+
+    return (
+        <>
+            <CardHeaderWithClose onClick={closeSidebar}>
+                <CardTitle className="text-xl">{selectedCountry?.name}</CardTitle>
+            </CardHeaderWithClose>
+            <CardContent className="flex h-[calc(100%_-_1rem_-_calc(45.6px_+_0.75rem))] flex-col gap-2 overflow-auto" />
+        </>
+    );
+};
+export default Country;
