1900384ac82c22077d9be2bedb0c4283bc4c60e3
Dunkelhaiser <103267805+Dunkelhaiser@users.noreply.github.com>
2025-03-10 23:03:18 +0200

    feat: use name field instead of name schema

--- src/shared/schemas/countries/createCountry.ts
+++ src/shared/schemas/countries/createCountry.ts
@@ -1,25 +1,23 @@
 import { z as zod } from "zod";
-import { audioSchema, imageSchema, nameSchema } from "../shared";
+import { audioSchema, imageSchema, nameField } from "../shared";
 
-export const createCountrySchema = zod
-    .object({
-        tag: zod
-            .string()
-            .trim()
-            .toUpperCase()
-            .regex(/^[A-Z]{3}$/, { message: "Enter valid tag" }),
-        color: zod
-            .string()
-            .trim()
-            .regex(/^#[0-9a-fA-F]{6}$/i, { message: "Enter valid color" }),
-        flag: imageSchema(),
-        coatOfArms: imageSchema(),
-        anthem: zod
-            .object({
-                url: audioSchema(),
-            })
-            .merge(nameSchema),
-    })
-    .merge(nameSchema);
+export const createCountrySchema = zod.object({
+    name: nameField({ field: "country", min: 1, max: 50 }),
+    tag: zod
+        .string()
+        .trim()
+        .toUpperCase()
+        .regex(/^[A-Z]{3}$/, { message: "Enter valid tag" }),
+    color: zod
+        .string()
+        .trim()
+        .regex(/^#[0-9a-fA-F]{6}$/i, { message: "Enter valid color" }),
+    flag: imageSchema(),
+    coatOfArms: imageSchema(),
+    anthem: zod.object({
+        name: nameField({ field: "anthem", min: 1, max: 50 }),
+        url: audioSchema(),
+    }),
+});
 
 export type CreateCountryInput = zod.infer<typeof createCountrySchema>;
--- src/shared/schemas/countries/updateCountry.ts
+++ src/shared/schemas/countries/updateCountry.ts
@@ -1,25 +1,23 @@
 import { z as zod } from "zod";
-import { audioSchema, imageSchema, nameSchema } from "../shared";
+import { audioSchema, imageSchema, nameField } from "../shared";
 
-export const updateCountrySchema = zod
-    .object({
-        tag: zod
-            .string()
-            .trim()
-            .toUpperCase()
-            .regex(/^[A-Z]{3}$/, { message: "Enter valid tag" }),
-        color: zod
-            .string()
-            .trim()
-            .regex(/^#[0-9a-fA-F]{6}$/i, { message: "Enter valid color" }),
-        flag: imageSchema({ optional: true }),
-        coatOfArms: imageSchema({ optional: true }),
-        anthem: zod
-            .object({
-                url: audioSchema({ optional: true }),
-            })
-            .merge(nameSchema),
-    })
-    .merge(nameSchema);
+export const updateCountrySchema = zod.object({
+    name: nameField({ field: "country", min: 1, max: 50 }),
+    tag: zod
+        .string()
+        .trim()
+        .toUpperCase()
+        .regex(/^[A-Z]{3}$/, { message: "Enter valid tag" }),
+    color: zod
+        .string()
+        .trim()
+        .regex(/^#[0-9a-fA-F]{6}$/i, { message: "Enter valid color" }),
+    flag: imageSchema({ optional: true }),
+    coatOfArms: imageSchema({ optional: true }),
+    anthem: zod.object({
+        name: nameField({ field: "anthem", min: 1, max: 50 }),
+        url: audioSchema({ optional: true }),
+    }),
+});
 
 export type UpdateCountryInput = zod.infer<typeof updateCountrySchema>;
