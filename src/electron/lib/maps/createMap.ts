d5095e14ed93edf34bc9ec556a6f215faac5a7dc
Dunkelhaiser <103267805+Dunkelhaiser@users.noreply.github.com>
2025-03-11 20:50:21 +0200

    feat: add backend data validation

--- src/electron/lib/maps/createMap.ts
+++ src/electron/lib/maps/createMap.ts
@@ -1,15 +1,18 @@
+import { CreateMapInput, createMapSchema } from "../../../shared/schemas/maps/createMap.js";
 import { db } from "../../db/db.js";
 import { maps } from "../../db/schema.js";
 import { processProvinces } from "../provinces/processProvinces.js";
 
-export const createMap = async (_: Electron.IpcMainInvokeEvent, name: string, imageData: string) => {
+export const createMap = async (_: Electron.IpcMainInvokeEvent, data: CreateMapInput) => {
+    const { name, provinces } = await createMapSchema.parseAsync(data);
+
     const newMap = await db.transaction(async (tx) => {
         const mapRes = await tx.insert(maps).values({ name }).returning();
 
         const createdMap = mapRes.length > 0 ? mapRes[0] : null;
 
         if (createdMap) {
-            await processProvinces(_, imageData, createdMap.id);
+            await processProvinces(_, provinces, createdMap.id);
         }
 
         return createdMap;
