641424e82b027f02a02707f2163c009cca25213f
Dunkelhaiser <103267805+Dunkelhaiser@users.noreply.github.com>
2025-03-12 20:59:32 +0200

    chore: add type error message

--- src/shared/schemas/alliances/createAlliance.ts
+++ src/shared/schemas/alliances/createAlliance.ts
@@ -3,7 +3,7 @@ import { nameField } from "../shared.js";
 
 export const createAllianceSchema = zod.object({
     name: nameField({ field: "alliance", min: 3, max: 50 }),
-    type: zod.enum(["economic", "military", "political"]),
+    type: zod.enum(["economic", "military", "political"], { message: "Select alliance type" }),
     leader: zod.string().trim().length(3).optional(),
 });
 
