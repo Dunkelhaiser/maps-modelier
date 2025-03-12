87f2ecb0b9435e5e73ba1fbe15a329f859f2ff4e
Dunkelhaiser <103267805+Dunkelhaiser@users.noreply.github.com>
2025-03-12 13:22:09 +0200

    feat: delete country folder on country deletion

--- src/electron/lib/countries/deleteCountry.ts
+++ src/electron/lib/countries/deleteCountry.ts
@@ -1,7 +1,9 @@
 import { and, eq } from "drizzle-orm";
 import { db } from "../../db/db.js";
 import { countries } from "../../db/schema.js";
+import { deleteFolder } from "../utils/deleteFolder.js";
 
 export const deleteCountry = async (_: Electron.IpcMainInvokeEvent, mapId: string, tag: string) => {
     await db.delete(countries).where(and(eq(countries.mapId, mapId), eq(countries.tag, tag)));
+    await deleteFolder(tag, ["media", mapId]);
 };
