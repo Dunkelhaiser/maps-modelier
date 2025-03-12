40a9ae2351c9c89e7f927385d44950207a02b39d
Dunkelhaiser <103267805+Dunkelhaiser@users.noreply.github.com>
2025-03-12 13:19:02 +0200

    feat: delete map folder on map deletion

--- src/electron/lib/maps/deleteMap.ts
+++ src/electron/lib/maps/deleteMap.ts
@@ -1,7 +1,9 @@
 import { eq } from "drizzle-orm";
 import { db } from "../../db/db.js";
 import { maps } from "../../db/schema.js";
+import { deleteFolder } from "../utils/deleteFolder.js";
 
 export const deleteMap = async (_: Electron.IpcMainInvokeEvent, id: string) => {
     await db.delete(maps).where(eq(maps.id, id));
+    await deleteFolder(id, ["media"]);
 };
