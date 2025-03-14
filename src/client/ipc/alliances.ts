81c06f11e7fdf77948c67416fe86c7a5b94f44ae
Dunkelhaiser <103267805+Dunkelhaiser@users.noreply.github.com>
2025-03-14 14:08:36 +0200

    feat: add edit alliance form

--- src/client/ipc/alliances.ts
+++ src/client/ipc/alliances.ts
@@ -1,12 +1,12 @@
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { toast } from "sonner";
-import { CreateAllianceInput } from "src/shared/schemas/alliances/createAlliance";
+import { AllianceInput } from "src/shared/schemas/alliances/alliance";
 
 export const useCreateAlliance = (mapId: string) => {
     const queryClient = useQueryClient();
 
     return useMutation({
-        mutationFn: async (data: CreateAllianceInput) => await window.electron.alliances.create(mapId, data),
+        mutationFn: async (data: AllianceInput) => await window.electron.alliances.create(mapId, data),
         onSuccess: () => {
             toast.success("Alliance created successfully");
             queryClient.invalidateQueries({ queryKey: [mapId, "alliances"] });
@@ -18,5 +18,17 @@ export const useGetAlliances = (mapId: string) => {
     return useQuery({
         queryKey: [mapId, "alliances"],
         queryFn: async () => await window.electron.alliances.getAll(mapId),
+    });
+};
+
+export const useUpdateAlliance = (mapId: string, id: number) => {
+    const queryClient = useQueryClient();
+
+    return useMutation({
+        mutationFn: async (data: AllianceInput) => await window.electron.alliances.update(mapId, id, data),
+        onSuccess: () => {
+            toast.success("Alliance updated successfully");
+            queryClient.invalidateQueries({ queryKey: [mapId, "alliances"] });
+        },
     });
 };
--- src/client/screens/MapView/components/UserInterface/Sidebar/AlliancesEditing/EditAllianceForm.tsx
+++ src/client/screens/MapView/components/UserInterface/Sidebar/AlliancesEditing/EditAllianceForm.tsx
@@ -1,4 +1,111 @@
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useActiveMap } from "@hooks/useActiveMap";
+import { useUpdateAlliance } from "@ipc/alliances";
+import { useGetCountries } from "@ipc/countries";
+import { useMapStore } from "@store/store";
+import { Button } from "@ui/Button";
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@ui/Form";
+import { Input } from "@ui/Input";
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@ui/Select";
+import { useForm } from "react-hook-form";
+import { AllianceInput, allianceSchema } from "src/shared/schemas/alliances/alliance";
+
 const EditAllianceForm = () => {
-    return <div>EditAllianceForm</div>;
+    const activeMap = useActiveMap();
+    const selectAlliance = useMapStore((state) => state.selectAlliance);
+    const selectedAlliance = useMapStore((state) => state.selectedAlliance)!;
+
+    const form = useForm<AllianceInput>({
+        resolver: zodResolver(allianceSchema),
+        defaultValues: {
+            name: selectedAlliance.name,
+            leader: selectedAlliance.leader.tag,
+            type: selectedAlliance.type as "economic" | "political" | "military",
+        },
+    });
+
+    const { data: countries } = useGetCountries(activeMap.id);
+    const updateAlliance = useUpdateAlliance(activeMap.id, selectedAlliance.id);
+
+    const updateAllianceHandler = async (data: AllianceInput) => {
+        const updatedAlliance = await updateAlliance.mutateAsync(data);
+        selectAlliance(updatedAlliance);
+    };
+
+    return (
+        <Form {...form}>
+            <form className="flex flex-col gap-4 py-4" onSubmit={form.handleSubmit(updateAllianceHandler)}>
+                <FormField
+                    control={form.control}
+                    name="name"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Name</FormLabel>
+                            <FormControl>
+                                <Input placeholder="Enter alliance name" {...field} />
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+                <div className="grid grid-cols-2 gap-2">
+                    <FormField
+                        control={form.control}
+                        name="type"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>Type</FormLabel>
+                                <FormControl>
+                                    <Select onValueChange={field.onChange} value={field.value}>
+                                        <SelectTrigger>
+                                            <SelectValue placeholder="Alliance Type" />
+                                        </SelectTrigger>
+                                        <SelectContent>
+                                            <SelectItem value="economic">Economic</SelectItem>
+                                            <SelectItem value="political">Political</SelectItem>
+                                            <SelectItem value="military">Military</SelectItem>
+                                        </SelectContent>
+                                    </Select>
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+                    <FormField
+                        control={form.control}
+                        name="leader"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>Leader</FormLabel>
+                                <FormControl>
+                                    <Select onValueChange={field.onChange} value={field.value}>
+                                        <SelectTrigger>
+                                            <SelectValue placeholder="Leader" />
+                                        </SelectTrigger>
+                                        <SelectContent>
+                                            {countries?.map((country) => (
+                                                <SelectItem key={country.tag} value={country.tag}>
+                                                    <div className="flex flex-row items-center gap-2">
+                                                        <img
+                                                            src={country.flag}
+                                                            alt={`${country.name} flag`}
+                                                            className="aspect-[3/2] h-4 rounded-sm object-cover"
+                                                        />
+                                                        {country.name}
+                                                    </div>
+                                                </SelectItem>
+                                            ))}
+                                        </SelectContent>
+                                    </Select>
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+                </div>
+                <Button isLoading={form.formState.isSubmitting}>Save</Button>
+            </form>
+        </Form>
+    );
 };
 export default EditAllianceForm;
