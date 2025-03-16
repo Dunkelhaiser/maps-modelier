46b385c2b0496fe8c6c29f7696f6645849780631
Dunkelhaiser <103267805+Dunkelhaiser@users.noreply.github.com>
2025-03-16 17:25:28 +0200

    chore: split ipc error part in toast

--- src/client/utils/errorToast.ts
+++ src/client/utils/errorToast.ts
@@ -2,6 +2,10 @@ import { toast } from "sonner";
 
 export const errorToast = async (err: unknown) => {
     if (err instanceof Error) {
+        if (err.message.startsWith("Error invoking remote method 'ipc': Error: ")) {
+            toast.error(err.message.split("Error invoking remote method 'ipc': Error: ")[1]);
+            return;
+        }
         toast.error(err.message);
         return;
     }
