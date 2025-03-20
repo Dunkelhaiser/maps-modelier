b27fdb8d3c68a0d0993d7a25fa9f0114b8b5c98d
Dunkelhaiser <103267805+Dunkelhaiser@users.noreply.github.com>
2025-03-20 15:15:02 +0200

    feat: make alliance tag clickable

--- src/client/screens/MapView/components/UserInterface/Sidebar/Country/AllianceTag.tsx
+++ src/client/screens/MapView/components/UserInterface/Sidebar/Country/AllianceTag.tsx
@@ -1,12 +1,21 @@
+import { useMapStore } from "@store/store";
 import { CountryAlliance } from "src/shared/types";
 
 interface Props {
     alliance: CountryAlliance;
 }
 
-export const AllianceTag = ({ alliance }: Props) => (
-    <div className="flex items-center gap-1 rounded-md bg-sky-100 px-2 py-1 text-sm text-sky-800">
-        <span>{alliance.name}</span>
-        <span className="rounded-full bg-sky-200 px-1.5 py-0.5 text-xs font-medium">{alliance.type}</span>
-    </div>
-);
+export const AllianceTag = ({ alliance }: Props) => {
+    const selectAlliance = useMapStore((state) => state.selectAlliance).bind(null, alliance.id);
+
+    return (
+        <button
+            type="button"
+            className="flex items-center gap-1 rounded-md bg-sky-100 px-2 py-1 text-sm text-sky-800"
+            onClick={selectAlliance}
+        >
+            <span>{alliance.name}</span>
+            <span className="rounded-full bg-sky-200 px-1.5 py-0.5 text-xs font-medium">{alliance.type}</span>
+        </button>
+    );
+};
