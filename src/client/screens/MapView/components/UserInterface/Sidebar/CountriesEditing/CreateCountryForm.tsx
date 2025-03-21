22656f9ded6fca27b78d5ce21ee2bc3ac97831a4
Dunkelhaiser <103267805+Dunkelhaiser@users.noreply.github.com>
2025-03-21 21:36:45 +0200

    chore: make country creation form resemble editing form

--- src/client/screens/MapView/components/UserInterface/Sidebar/CountriesEditing/CreateCountryForm.tsx
+++ src/client/screens/MapView/components/UserInterface/Sidebar/CountriesEditing/CreateCountryForm.tsx
@@ -6,6 +6,7 @@ import { Button } from "@ui/Button";
 import FileUpload from "@ui/FileUpload";
 import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@ui/Form";
 import { Input } from "@ui/Input";
+import { useRef } from "react";
 import { useForm } from "react-hook-form";
 import { CreateCountryInput, createCountrySchema } from "src/shared/schemas/countries/createCountry";
 
@@ -21,6 +22,7 @@ const CreateCountryForm = () => {
         },
     });
     const flagRef = form.register("flag");
+    const colorPickerRef = useRef<HTMLInputElement>(null);
 
     const createCountry = useCreateCountry(activeMap);
 
@@ -31,30 +33,49 @@ const CreateCountryForm = () => {
 
     return (
         <Form {...form}>
-            <form className="flex flex-col gap-4 py-4" onSubmit={form.handleSubmit(createCountryHandler)}>
-                <FormField
-                    control={form.control}
-                    name="name"
-                    render={({ field }) => (
-                        <FormItem>
-                            <FormLabel>Name</FormLabel>
-                            <FormControl>
-                                <Input placeholder="Enter country name" {...field} />
-                            </FormControl>
-                            <FormMessage />
-                        </FormItem>
-                    )}
-                />
-                <div className="flex flex-row gap-4">
+            <form className="flex flex-col gap-4" onSubmit={form.handleSubmit(createCountryHandler)}>
+                <div className="flex items-center gap-2">
+                    <button
+                        type="button"
+                        className="size-6 rounded-full"
+                        style={{ backgroundColor: form.watch("color") }}
+                        onClick={() => colorPickerRef.current?.click()}
+                    />
+                    <FormField
+                        control={form.control}
+                        name="color"
+                        render={({ field }) => (
+                            <FormItem className="invisible size-0">
+                                <FormControl>
+                                    <Input type="color" {...field} ref={colorPickerRef} />
+                                </FormControl>
+                            </FormItem>
+                        )}
+                    />
+                    <FormField
+                        control={form.control}
+                        name="name"
+                        render={({ field }) => (
+                            <FormItem className="flex-1">
+                                <FormLabel>Common name</FormLabel>
+                                <FormControl>
+                                    <Input placeholder="Enter common name" {...field} />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+                </div>
+                <div className="flex gap-3">
                     <FormField
                         control={form.control}
                         name="flag"
                         render={() => (
-                            <FormItem>
+                            <FormItem className="flex-1">
                                 <FormLabel>Flag</FormLabel>
                                 <FormControl>
                                     <FileUpload
-                                        className="aspect-[3/2] h-36"
+                                        className="h-32"
                                         {...flagRef}
                                         accept="image/png, image/jpg, image/jpeg, image/webp, image/bmp"
                                     />
@@ -63,20 +84,8 @@ const CreateCountryForm = () => {
                             </FormItem>
                         )}
                     />
+                    <div className="aspect-square h-32 w-auto flex-1" />
                 </div>
-                <FormField
-                    control={form.control}
-                    name="color"
-                    render={({ field }) => (
-                        <FormItem>
-                            <FormLabel>Color</FormLabel>
-                            <FormControl>
-                                <Input type="color" className="h-8 w-12" {...field} />
-                            </FormControl>
-                            <FormMessage />
-                        </FormItem>
-                    )}
-                />
                 <Button isLoading={form.formState.isSubmitting}>Create Country</Button>
             </form>
         </Form>
