d4d47700bff4a069759fc3a1f9feec965320deec
Dunkelhaiser <103267805+Dunkelhaiser@users.noreply.github.com>
2025-03-22 16:46:46 +0200

    feat: add alliance deletion button

--- src/client/screens/MapView/components/UserInterface/Sidebar/AlliancesEditing/EditAllianceForm.tsx
+++ src/client/screens/MapView/components/UserInterface/Sidebar/AlliancesEditing/EditAllianceForm.tsx
@@ -1,12 +1,12 @@
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useActiveMap } from "@hooks/useActiveMap";
-import { useGetAlliance, useGetMembers, useUpdateAlliance } from "@ipc/alliances";
+import { useDeleteAlliance, useGetAlliance, useGetMembers, useUpdateAlliance } from "@ipc/alliances";
 import { useMapStore } from "@store/store";
 import { Button } from "@ui/Button";
 import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@ui/Form";
 import { Input } from "@ui/Input";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@ui/Select";
-import { Save } from "lucide-react";
+import { Save, Trash } from "lucide-react";
 import { useEffect, useMemo } from "react";
 import { useForm } from "react-hook-form";
 import { AllianceInput, allianceSchema } from "src/shared/schemas/alliances/alliance";
@@ -44,6 +44,13 @@ const EditAllianceForm = () => {
     const updateAllianceHandler = async (data: AllianceInput) => {
         const updatedAlliance = await updateAlliance.mutateAsync(data);
         selectAlliance(updatedAlliance.id);
+    };
+
+    const deleteAlliance = useDeleteAlliance(activeMap, selectedAlliance);
+
+    const deleteAllianceHandler = async () => {
+        await deleteAlliance.mutateAsync();
+        selectAlliance(null);
     };
 
     return (
@@ -118,10 +125,25 @@ const EditAllianceForm = () => {
                             )}
                         />
                     </div>
-                    <Button isLoading={form.formState.isSubmitting} className="flex-1 gap-2" disabled={isFormUnchanged}>
-                        <Save />
-                        Save Changes
-                    </Button>
+                    <div className="mt-6 flex gap-2">
+                        <Button
+                            isLoading={form.formState.isSubmitting}
+                            className="flex-1 gap-2"
+                            disabled={isFormUnchanged}
+                        >
+                            <Save />
+                            Save Changes
+                        </Button>
+                        <Button
+                            type="button"
+                            variant="destructive"
+                            aria-label="Delete Country"
+                            onClick={deleteAllianceHandler}
+                            isLoading={deleteAlliance.isPending}
+                        >
+                            <Trash />
+                        </Button>
+                    </div>
                 </form>
             </Form>
             <AddMembersForm members={members} leaderId={alliance?.leader.id} />
