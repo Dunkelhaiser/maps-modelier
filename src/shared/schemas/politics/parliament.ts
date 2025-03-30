24614a90e6a9e0058efb1e67a057c13e3a780779
Dunkelhaiser <103267805+Dunkelhaiser@users.noreply.github.com>
2025-03-30 23:36:58 +0300

    feat: add check if leaders are not the same

--- src/shared/schemas/politics/parliament.ts
+++ src/shared/schemas/politics/parliament.ts
@@ -1,11 +1,20 @@
 import { z as zod } from "zod";
 import { nameField } from "../shared.js";
 
-export const parliamentSchema = zod.object({
-    name: nameField({ field: "parliament", min: 1, max: 50 }),
-    seatsNumber: zod.coerce.number().int().min(1, { message: "Provide number of seats" }),
-    oppositionLeaderId: zod.coerce.number().int({ message: "Select opposition leader" }).optional(),
-    coalitionLeaderId: zod.coerce.number().int({ message: "Select coalition leader" }).optional(),
-});
+export const parliamentSchema = zod
+    .object({
+        name: nameField({ field: "parliament", min: 1, max: 50 }),
+        seatsNumber: zod.coerce.number().int().min(1, { message: "Provide number of seats" }),
+        oppositionLeaderId: zod.coerce.number().int({ message: "Select opposition leader" }).optional(),
+        coalitionLeaderId: zod.coerce.number().int({ message: "Select coalition leader" }).optional(),
+    })
+    .refine(
+        (data) =>
+            !(data.oppositionLeaderId && data.coalitionLeaderId && data.oppositionLeaderId === data.coalitionLeaderId),
+        {
+            message: "Coalition and opposition leaders cannot be the same",
+            path: ["oppositionLeaderId"],
+        }
+    );
 
 export type ParliamentInput = zod.infer<typeof parliamentSchema>;
