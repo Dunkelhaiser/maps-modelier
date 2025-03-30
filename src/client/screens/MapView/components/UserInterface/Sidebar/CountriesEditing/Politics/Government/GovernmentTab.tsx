d60326e37fe021adf07de8c35d08100e629f0796
Dunkelhaiser <103267805+Dunkelhaiser@users.noreply.github.com>
2025-03-30 21:36:47 +0300

    feat: add parliament creation form

--- src/client/screens/MapView/components/UserInterface/Sidebar/CountriesEditing/Politics/Government/GovernmentTab.tsx
+++ src/client/screens/MapView/components/UserInterface/Sidebar/CountriesEditing/Politics/Government/GovernmentTab.tsx
@@ -1,12 +1,16 @@
 import { TabsContent } from "@ui/Tabs";
 import HeadOfGovernmentForm from "./HeadOfGovernmentForm";
 import HeadOfStateForm from "./HeadOfStateForm";
+import ParliamentForm from "./ParliamentForm";
 
 const GovernmentTab = () => {
     return (
-        <TabsContent value="government" className="grid grid-cols-2 gap-4">
-            <HeadOfStateForm />
-            <HeadOfGovernmentForm />
+        <TabsContent value="government" className="space-y-4">
+            <div className="grid grid-cols-2 gap-4">
+                <HeadOfStateForm />
+                <HeadOfGovernmentForm />
+            </div>
+            <ParliamentForm />
         </TabsContent>
     );
 };
--- src/shared/schemas/politics/parliament.ts
+++ src/shared/schemas/politics/parliament.ts
@@ -3,7 +3,7 @@ import { nameField } from "../shared.js";
 
 export const parliamentSchema = zod.object({
     name: nameField({ field: "parliament", min: 1, max: 50 }),
-    seatsNumber: zod.number().int().min(1, { message: "Provide number of seats" }),
+    seatsNumber: zod.coerce.number().int().min(1, { message: "Provide number of seats" }),
 });
 
 export type ParliamentInput = zod.infer<typeof parliamentSchema>;
