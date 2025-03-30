7c38e106b791f477c03e064c7e09a715844b3b00
Dunkelhaiser <103267805+Dunkelhaiser@users.noreply.github.com>
2025-03-30 21:36:24 +0300

    chore: check if parliament already exists

--- src/electron/lib/government/createParliament.ts
+++ src/electron/lib/government/createParliament.ts
@@ -1,3 +1,4 @@
+import { and, eq } from "drizzle-orm";
 import { ParliamentInput, parliamentSchema } from "../../../shared/schemas/politics/parliament.js";
 import { db } from "../../db/db.js";
 import { parliaments } from "../../db/schema.js";
@@ -9,6 +10,13 @@ export const createParliament = async (
     data: ParliamentInput
 ) => {
     const parsedData = await parliamentSchema.parseAsync(data);
+
+    const existingParliament = await db
+        .select()
+        .from(parliaments)
+        .where(and(eq(parliaments.mapId, mapId), eq(parliaments.countryId, countryId)));
+
+    if (existingParliament.length > 0) throw new Error("Parliament already exists");
 
     const [createdParliament] = await db
         .insert(parliaments)
