4ceda99f7afe83f247c82c5ad2e8e5ecc27cd11e
Dunkelhaiser <103267805+Dunkelhaiser@users.noreply.github.com>
2025-03-30 12:24:55 +0300

    feat: add schema for head of state/government

---
+++ src/shared/schemas/politics/assignHead.ts
@@ -0,0 +1,11 @@
+import { z as zod } from "zod";
+import { nameField } from "../shared";
+
+export const assignHeadSchema = zod.object({
+    head: zod.coerce.number().int({ message: "Select head" }),
+    title: nameField({ field: "title", min: 3, max: 50 }),
+    startDate: zod.coerce.date({ message: "Provide date the head assumed position" }),
+    endDate: zod.coerce.date({ message: "Provide expected end term date" }).optional(),
+});
+
+export type AssignHeadInput = zod.infer<typeof assignHeadSchema>;
