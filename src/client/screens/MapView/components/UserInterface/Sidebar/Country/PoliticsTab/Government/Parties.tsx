2953e8e3d87676e2ecbe89bfe9a58ca421bae969
Dunkelhaiser <103267805+Dunkelhaiser@users.noreply.github.com>
2025-03-31 16:23:10 +0300

    chore: do not display empty sides

--- src/client/screens/MapView/components/UserInterface/Sidebar/Country/PoliticsTab/Government/Parties.tsx
+++ src/client/screens/MapView/components/UserInterface/Sidebar/Country/PoliticsTab/Government/Parties.tsx
@@ -21,33 +21,36 @@ const Parties = ({ groups }: Props) => {
 
     const selectParty = useMapStore((state) => state.selectParty);
 
-    return groups.map((group) => (
-        <div key={group.side} className="flex flex-col gap-2">
-            <div className="flex items-center gap-2">
-                <h4 className="text-sm font-medium">{getGroupTitle(group.side)}</h4>{" "}
-                <span className="text-xs text-muted-foreground">{group.parties.length} parties</span>
-                <span className="text-xs text-muted-foreground">
-                    • {group.parties.reduce((acc, party) => acc + party.seats, 0)} seats
-                </span>
-            </div>
-            <div className="grid gap-2">
-                {group.parties.map((party) => (
-                    <button
-                        key={party.id}
-                        className="flex items-center justify-between rounded-md border p-2"
-                        type="button"
-                        onClick={() => selectParty(party.id)}
-                    >
-                        <div className="flex items-center gap-2">
-                            <div className="size-4 rounded-full" style={{ backgroundColor: party.color }} />
-                            <span className="text-sm">{party.acronym ?? party.name}</span>
-                        </div>
-                        <span className="text-sm font-medium">{party.seats} seats</span>
-                    </button>
-                ))}
-            </div>
-        </div>
-    ));
+    return groups.map(
+        (group) =>
+            group.parties.length > 0 && (
+                <div key={group.side} className="flex flex-col gap-2">
+                    <div className="flex items-center gap-2">
+                        <h4 className="text-sm font-medium">{getGroupTitle(group.side)}</h4>{" "}
+                        <span className="text-xs text-muted-foreground">{group.parties.length} parties</span>
+                        <span className="text-xs text-muted-foreground">
+                            • {group.parties.reduce((acc, party) => acc + party.seats, 0)} seats
+                        </span>
+                    </div>
+                    <div className="grid gap-2">
+                        {group.parties.map((party) => (
+                            <button
+                                key={party.id}
+                                className="flex items-center justify-between rounded-md border p-2"
+                                type="button"
+                                onClick={() => selectParty(party.id)}
+                            >
+                                <div className="flex items-center gap-2">
+                                    <div className="size-4 rounded-full" style={{ backgroundColor: party.color }} />
+                                    <span className="text-sm">{party.acronym ?? party.name}</span>
+                                </div>
+                                <span className="text-sm font-medium">{party.seats} seats</span>
+                            </button>
+                        ))}
+                    </div>
+                </div>
+            )
+    );
 };
 
 export default Parties;
