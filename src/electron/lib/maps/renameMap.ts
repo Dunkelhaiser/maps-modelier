3bafb45f2b70236da6970b7bd668f284621abe4e
Dunkelhaiser <103267805+Dunkelhaiser@users.noreply.github.com>
2025-04-02 22:36:01 +0300

    refactor: simplify return statement

--- src/electron/lib/maps/renameMap.ts
+++ src/electron/lib/maps/renameMap.ts
@@ -5,6 +5,6 @@ import { maps } from "../../db/schema.js";
 
 export const renameMap = async (_: Electron.IpcMainInvokeEvent, id: string, data: RenameMapInput) => {
     const { name } = await renameMapSchema.parseAsync(data);
-    const renamedMap = await db.update(maps).set({ name }).where(eq(maps.id, id)).returning();
-    return renamedMap.length > 0 ? renamedMap[0] : null;
+    const [renamedMap] = await db.update(maps).set({ name }).where(eq(maps.id, id)).returning();
+    return renamedMap;
 };
