545e81e51a611b4d17ec17b15cbe5df0f9045205
Dunkelhaiser <103267805+Dunkelhaiser@users.noreply.github.com>
2025-04-02 22:03:06 +0300

    refactor: remove old provinces image system leftovers

--- src/client/screens/MapSelection/components/MapButton.tsx
+++ src/client/screens/MapSelection/components/MapButton.tsx
@@ -1,11 +1,9 @@
 import { useMapStore } from "@store/store";
 import { formatDate } from "@utils/utils";
 import { MapIcon } from "lucide-react";
-import { useState } from "react";
 import { MapType } from "src/shared/types";
 import DeleteMapDialog from "./DeleteMapDialog";
 import RenameMapDialog from "./RenameMap/RenameMapDialog";
-import UploadMapImageDialog from "./UploadMapImage/UploadMapImageDialog";
 
 interface Props {
     map: MapType;
@@ -13,36 +11,29 @@ interface Props {
 
 const MapButton = ({ map }: Props) => {
     const setActiveMap = useMapStore((state) => state.setActiveMap);
-    const [selectedMapForUpload, setSelectedMapForUpload] = useState<MapType | null>(null);
 
     const handleExistingMapSelect = async () => {
         setActiveMap(map.id);
     };
 
     return (
-        <>
-            <div className="relative flex h-auto w-full cursor-pointer items-center justify-center gap-2 rounded-md border border-input bg-background px-4 py-2 shadow-sm transition focus-within:translate-y-[2px] focus-within:ring-2 focus-within:ring-ring hover:translate-y-[2px] hover:bg-accent hover:text-accent-foreground">
-                <MapIcon className="mr-2 size-5" />
-                <div className="flex flex-col items-start">
-                    <button
-                        type="button"
-                        onClick={handleExistingMapSelect}
-                        className="text-sm font-medium outline-none before:absolute before:inset-0 before:content-['']"
-                    >
-                        {map.name}
-                    </button>
-                    <span className="text-xs text-muted-foreground">Created: {formatDate(map.createdAt)}</span>
-                </div>
-                <div className="z-10 ml-auto space-x-0.5">
-                    <RenameMapDialog map={map} />
-                    <DeleteMapDialog map={map} />
-                </div>
+        <div className="relative flex h-auto w-full cursor-pointer items-center justify-center gap-2 rounded-md border border-input bg-background px-4 py-2 shadow-sm transition focus-within:translate-y-[2px] focus-within:ring-2 focus-within:ring-ring hover:translate-y-[2px] hover:bg-accent hover:text-accent-foreground">
+            <MapIcon className="mr-2 size-5" />
+            <div className="flex flex-col items-start">
+                <button
+                    type="button"
+                    onClick={handleExistingMapSelect}
+                    className="text-sm font-medium outline-none before:absolute before:inset-0 before:content-['']"
+                >
+                    {map.name}
+                </button>
+                <span className="text-xs text-muted-foreground">Created: {formatDate(map.createdAt)}</span>
             </div>
-            <UploadMapImageDialog
-                selectedMapForUpload={selectedMapForUpload}
-                setSelectedMapForUpload={setSelectedMapForUpload}
-            />
-        </>
+            <div className="z-10 ml-auto space-x-0.5">
+                <RenameMapDialog map={map} />
+                <DeleteMapDialog map={map} />
+            </div>
+        </div>
     );
 };
 export default MapButton;
--- src/shared/schemas/mapImage/upload.ts
+++
@@ -1,8 +0,0 @@
-import { z as zod } from "zod";
-import { imageSchema } from "../shared.js";
-
-export const uploadMapImageSchema = zod.object({
-    provinces: imageSchema(),
-});
-
-export type UploadeMapImageInput = zod.infer<typeof uploadMapImageSchema>;
