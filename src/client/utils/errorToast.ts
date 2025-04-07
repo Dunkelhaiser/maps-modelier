85a57956e3a3f59505357e212f37450d41d885ad
Dunkelhaiser <103267805+Dunkelhaiser@users.noreply.github.com>
2025-04-07 20:37:11 +0300

    refactor: extract error messages to variables

--- src/client/utils/errorToast.ts
+++ src/client/utils/errorToast.ts
@@ -2,16 +2,21 @@ import { toast } from "sonner";
 
 export const errorToast = async (err: unknown) => {
     if (err instanceof Error) {
-        if (err.message.startsWith("Error invoking remote method 'ipc': Error: ")) {
-            toast.error(err.message.split("Error invoking remote method 'ipc': Error: ")[1]);
+        const ipcErrorMsg = "Error invoking remote method 'ipc': Error: ";
+        if (err.message.startsWith(ipcErrorMsg)) {
+            toast.error(err.message.split(ipcErrorMsg)[1]);
             return;
         }
-        if (err.message.startsWith("Error invoking remote method 'ipc': ")) {
-            toast.error(err.message.split("Error invoking remote method 'ipc': ")[1]);
+
+        const ipcErrorMsg2 = "Error invoking remote method 'ipc': ";
+        if (err.message.startsWith(ipcErrorMsg2)) {
+            toast.error(err.message.split(ipcErrorMsg2)[1]);
             return;
         }
+
         toast.error(err.message);
         return;
     }
+
     toast.error("Failed to perform the operation");
 };
